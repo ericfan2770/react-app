51a726c57809b97ef2dd2b47ab31a86e20bff5a2
Eric Fan <ericfan2770@gmail.com>
2020-03-22 22:43:36 -0700

    Part 1: Hello World

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const element = <h1>Hello World</h1>;
+// console.log(element);
+
+// 2nd arg: where in real DOM we'll render
 ReactDOM.render(element, document.getElementById("root"));
